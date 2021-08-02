3eaf29077dcce52bda2e46cea6c514186e4d92b6
rjbergerud <rjbergerud@users.noreply.github.com>
2021-08-02 06:16:50 +0000

    deploy: c77668f1e7fc2f2697ae7a127f4105e64edddaa9

---
+++ _next/static/RX46XsfTybgtR15O6fv_2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f6d0aacc69d0f60022c2.css","static/chunks/pages/index-cda648ecfc49022e94cf.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
